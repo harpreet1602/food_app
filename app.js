1205117b8a27c72ddb7aa29a47296b87a313a8a9
harpreet1602 <harpreets1602@gmail.com>
2021-09-29 22:58:56 +0530

    update

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 
 const express = require('express');
+// server is like a restaurant and client is the customer
+// customer requests something from the restaurant and get a response.
 
 // server creation
 const app = express();
@@ -31,7 +33,9 @@ app.get("/home",function(req,res){
     console.log(__dirname);
     res.sendFile("./views/index.html",{root:__dirname});
 });
-
+app.use(express.json());
+// after using the express.json format then only we will get data in post method from req.body
+// in post we can put some data and that changed data will be fetched here.
 app.post('/users',(req,res)=>{
     user = req.body;
     console.log(req.body);
